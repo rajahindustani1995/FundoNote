2c23fd23612f528a1168436b968e0b6dbb751c69
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-09-01 07:15:54 +0530

    [Rajesh] Added : Created Label Table Successfully

---
+++ FundoNote/FundoNote/Controllers/LabelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FundoNote.Controllers
+{
+    public class LabelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
